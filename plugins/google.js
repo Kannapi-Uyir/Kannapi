f1465877932f4621086ae07e592f9ad8d9f94aec
Kannapi-Uyir <83867438+Kannapi-Uyir@users.noreply.github.com>
2021-05-11 15:32:41 +0530

    Update google.js

--- plugins/google.js
+++ plugins/google.js
@@ -3,7 +3,7 @@ let googleIt = require('google-it')
 let handler = async (m, { conn, command, args }) => {
   let full = /f$/i.test(command)
   let text = args.join` `
-  if (!text) return conn.reply(m.chat, 'There is no text to search', m)
+  if (!text) return conn.reply(m.chat, '*Must Enter Some Words To Search*', m)
   let url = 'https://google.com/search?q=' + encodeURIComponent(text)
   let search = await googleIt({ query: text })
   let msg = search.map(({ title, link, snippet}) => {
